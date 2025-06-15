a69210f400e778a8f0e90b6bbc9f274710c75d86
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-06-16 00:16:38 +0300

    Working

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ensure this is correct for your deployment
-  server: {
-    open: true, // optional: opens browser automatically
-  },
-  build: {
-    outDir: 'dist',
-  },
-  // fix 404 on refresh with react-router
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+    plugins: [react()],
+    server: {
+        port: 5174
+    }
 })
